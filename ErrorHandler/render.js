f16daf075a8c6bdbe177b11c125b524ef361a188
nam <cnam0303@gmail.com>
2019-04-24 09:09:44 +0700

    render

---
+++ ErrorHandler/render.js
@@ -0,0 +1,11 @@
+class errorHandler {
+    renderError () {
+        if (this.specifyError() != '') {
+            let position = 'top-left'
+            let timeout = 3
+            let isStacked = 'false'
+            let alertError = new Alert(position,timeout,isStacked)
+            alertError.error()
+        }
+    }
+}
