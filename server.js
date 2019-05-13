11bb3a96276dc51bb89dd6cf0f0e7a0875a5bda9
Nguyen Huu Quyen <huuquyen1412@gmail.com>
2019-05-13 15:17:21 +0700

    add function
    still bug

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 var http = require('http')
 var fs = require('fs')
-var taskList = [
-    {
-       taskName : 'do sth 1',
-       checked: false
-    }, 
-    {
-        taskName : 'do sth 2',
-        checked: false
-    }, 
-    {
-        taskName : 'do sth 3',
-        checked: false
-    },
-    {
-        taskName : 'do sth 4',
-        checked: true
-    }
-]
+
+//thử nghiệm nếu có sẵn 4 task như sau
+var taskList = []
 
 function getResources(req,res) {
-    // console.log(req.url)
-    if(req.url == '/' ) {
-       res.writeHead(200,{'Content-Type':'text/html'})
-       fs.createReadStream(__dirname + '/index.html').pipe(res)
+    if(req.url == '/' || req.url == '/all' || req.url == '/done' || req.url == '/undone') {
+        res.writeHead(200,{'Content-Type':'text/html'})
+        fs.createReadStream(__dirname + '/index.html').pipe(res)
     }
     if(req.url ==='/style.css' ) {
         res.writeHead(200,{'Content-Type':'text/css'})
@@ -41,8 +25,62 @@ function getResources(req,res) {
 
 http.createServer((req,res) => {
     getResources(req,res)
-    if(req.url == '/delete') {
+    if(req.url === '/add-task') {
+        req.on('data', reqData => {
+            const newTask = { taskName: JSON.parse(reqData.toString()), checked: false }
+            taskList.push(newTask)
+            res.end(JSON.stringify(newTask))
+        })
     }
+    if(req.url === '/delete') {
+        req.on('data', result => {
+            currentIndex = JSON.parse(result.toString())
+            taskList.splice(currentIndex,1)
+            res.end()
+    })
+    }
+    if(req.url === '/checkTask') {
+        let index = 0
+        req.on('data', result => {
+          let checkedIndexes = JSON.parse(result.toString())
+          for(let i = 0 ; i < taskList.length; i++) {
+            if(i === checkedIndexes[index]) {
+                index++
+                taskList[i].checked = true
+            }
+            else {
+                taskList[i].checked = false
+            }
+          }
+          res.writeHead(200,{'Content-Type':'text/plain'})
+          res.end(JSON.stringify(taskList))
+        })
+    }
+    if(req.url === '/all') {
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.end(JSON.stringify(taskList))
+    }
+    if(req.url === '/done') {
+        let doneTasks = []
+        for(let i = 0 ; i < taskList.length; i++) {
+            if(taskList[i].checked === true) {
+                doneTasks.push(taskList[i])
+            }
+        }
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.end(JSON.stringify(doneTasks))
+    }
+    if(req.url === '/undone') {
+        let undoneTasks = []
+        for(let i = 0 ; i < taskList.length; i++) {
+            if(taskList[i].checked === false) {
+                undoneTasks.push(taskList[i])
+            }
+        }
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.end(JSON.stringify(undoneTasks))
+    }
+
 }).listen(3000)
 
 console.log('Sever is now running on http://localhost:3000')
