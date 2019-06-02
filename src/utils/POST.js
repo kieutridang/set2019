3f972d4ae4b5ee4254d6a7cecb146f339bde322c
Vu Thien <St.violent@gmail.com>
2019-06-02 22:37:47 +0700

    update

--- src/utils/POST.js
+++ src/utils/POST.js
@@ -1,14 +1,14 @@
 const getRequest = function(url) {
     return new Promise((resolve,reject) => {
         var get = new XMLHttpRequest()
-        get.open('GET', url)
+        get.open('POST', url)
         get.send()
         get.onload = function() {
             let data = JSON.parse(get.responseText)
             resolve(data)
         }
         get.onerror = function() {
-            reject('get failed')
+            reject('post failed')
         }
     })
 }
